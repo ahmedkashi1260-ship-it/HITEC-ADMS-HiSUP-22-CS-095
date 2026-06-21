203f4aec063e04ba06b744b551e19743341cd87a
ahmedkashi1260-ship-it <ahmedkashi1260@gmail.com>
2026-06-21 13:33:29 +0000

    [Auth] Scaffold ASP.NET Identity UI for Login, Register, Logout

--- src/HiSUP/Program.cs
+++ src/HiSUP/Program.cs
@@ -1,6 +1,7 @@
 using HiSUP.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using HiSUP.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
